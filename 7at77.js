978dcd4374c51a99af445598d04519ef400e40ef
Emma Winston <emma.winston@gmail.com>
2018-09-17 17:03:44 +0100

    future function

--- 7at77.js
+++ 7at77.js
@@ -3,12 +3,28 @@ Gibber.scale.mode = 'Phrygian'
 E = Euclid
 
 a = FM({ attack:ms(10), decay:ms(1000), maxVoices: 3})
-	.amp(.2)
-	.note.seq( [0,2], 1/16 )
+r = Reverb({ roomSize: Add( .78, Sine( .05, .245 )._ ) })
+a.fx.add( r )
+
+future( function() {
+
+s = Seq({
+  note:[0,2],
+  durations:[1/16],
+  amp:[.2],
+  pan:[-0.6, -0.4, -0.2, 0, 0.3, 0.6, 0.3, 0, -0.4],
+  target:a})
+
+}, 2);
+
+
+
+
+
+
+d = Drums( 'xoxo')
+	.amp(.3)
 
 b = SoundFont( 'kalimba' )
 	.amp(.8)
 	.chord.seq( Rndi(0,8,3), E( 5,9,1/16 ) )
-
-d = Drums( 'xoxo')
-	.amp(.3)
