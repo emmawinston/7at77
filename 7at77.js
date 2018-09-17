d6dea3943971c811262e5b8dd8bc698bb00d89d0
Emma Winston <emma.winston@gmail.com>
2018-09-17 16:51:53 +0100

    euclid stuff

--- 7at77.js
+++ 7at77.js
@@ -1,11 +1,13 @@
 Gibber.scale.mode = 'Phrygian'
 
-a = FM({ maxVoices: 3})
+E = Euclid
+
+a = FM({ attack:ms(10), decay:ms(1000), maxVoices: 3})
 	.amp(.2)
-	.note.seq()
+	.note.seq( [0,2], 1/16 )
 
 b = SoundFont( 'kalimba' )
-	.amp(.5)
+	.amp(.8)
 	.chord.seq( Rndi(0,8,3), E( 5,9,1/16 ) )
 
 d = Drums( 'xoxo')
