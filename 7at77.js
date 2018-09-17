afbd268eed102cd97e8615c58a6f7befb347201a
Emma Winston <emma.winston@gmail.com>
2018-09-17 17:45:02 +0100

    two FMs and a pluck

--- 7at77.js
+++ 7at77.js
@@ -1,7 +1,7 @@
 Gibber.scale.mode = 'Phrygian'
 E = Euclid
-a = FM({ attack:ms(10), decay:ms(1000), maxVoices: 4})
-b = Pluck()
+aa = FM({ attack:ms(10), decay:ms(1000), maxVoices: 6})
+c = Pluck()
 r = Reverb({ roomSize: Add( .75, Sine( .05, .245 )._ ) })
 a.fx.add( r )
 
@@ -12,23 +12,35 @@ score = Score([
   s = Seq({
     note:[0,2],
     durations:[1/16],
-    amp:[.2],
+    amp:[.02],
     pan:[-0.2, 0, 0.2, 0],
-    target:a})
+    target:aa})
 
   console.log('pattern 1')
   },
-  measures(2), // wait 8 measures, then execute next function
+  measures(8), // wait 8 measures, then execute next function
   function() {
 
   t = Seq({
+    note:[2,4],
+    durations:[1/16],
+    amp:[.02],
+    pan:[0.2, 0.1, -0.2, -0.1],
+    target:aa})
+
+  console.log('pattern 2')
+  },
+  measures(8), // wait 8 measures, then execute next function
+  function() {
+
+  u = Seq({
     note:[0,2,4,5],
     durations:[1/16],
-    amp:[.8],
+    amp:[.1],
     pan:[-0.6, -0.4, -0.2, 0, 0.3, 0.6, 0.3, 0, -0.4],
     target:b})
     
-  console.log('pattern 2')
+  console.log('pluck')
   }
 ]).start()
 
@@ -45,3 +57,6 @@ d = Drums( 'xoxo')
 b = SoundFont( 'kalimba' )
 	.amp(.8)
 	.chord.seq( Rndi(0,8,3), E( 5,9,1/16 ) )
+
+
+// 1732 bars in 77 minutes
